5e29f85c7d1067ee6e38fc768156728bf07691e1
Theo Carvalho <theo@MacbBook-Pro.local>
2021-02-15 14:23:02 -0300

    First commit, added REST API to access items, stores, and userauthentication.

---
+++ app.py
@@ -0,0 +1,43 @@
+from db import db
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList 
+
+
+# JWT = Json Web Token
+
+# para iniciar virtual env source venv/bin/activate
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' #podemos colocar qualquer tipo de SQL aqui. poderia ser por exemplo postgres seria apenas mudar isso aqui.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'secret_key'
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity) # JWT creates a new endpoint /auth. when we call /auth temos que enviar um username e um password
+
+
+api.add_resource(Store,'/store/<string:name>')
+api.add_resource(Item,'/item/<string:name>') # http://127.0.0.1:5000/item/Rolf
+api.add_resource(ItemList,'/items')
+api.add_resource(StoreList, '/stores')
+api.add_resource(UserRegister,'/register')
+
+
+db.init_app(app)
+app.run(port=5000, debug=True)
+
+
+
+if __name__ == '__main__': # isso daqui serve para prevenir caso algum file rode um import app ele não rode um app.run. o __main__ é o nome que o python atribui ao arquivo da onde surgiu a execução do run
+    
+    app.run(port=5000, debug=True)
---
+++ models/user.py
@@ -0,0 +1,28 @@
+# a model is a internal representation of a entity. resources são external representation of an entity. recursos que a api responde para o cliente como nome da loja, preço, nome do item etc
+import sqlite3
+from db import db
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    password = db.Column(db.String(80))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+    @classmethod
+    def find_by_username(cls,username):
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def find_by_id(cls,_id):
+        return cls.query.filter_by(id=_id).first()
